b9d28936756e1ac80d25165e5b93c36032440b49
cristina <cristina.ilea2@yahoo.com>
2023-07-05 20:49:40 +0300

    code 5 iulie

---
+++ src/test/java/com/herokuapp/Logout.java
@@ -0,0 +1,14 @@
+package com.herokuapp;
+
+import org.testng.annotations.Test;
+
+public class Logout {
+    @Test
+    public void logoutTest(){
+        //open
+        //login
+        //logout
+        //verify successfull logout
+        //close page
+    }
+}
